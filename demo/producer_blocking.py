1da1ad4cab49f8c8884a07b652a6b46f96d5b75a
manutest1712 <manugithub17121980@gmail.com>
2025-11-28 14:02:16 +0530

    added topic creation parameters and topic existence check

--- demo/producer_blocking.py
+++ demo/producer_blocking.py
@@ -17,7 +17,21 @@ def delivery_report(err, msg):
 
 def create_topic():
     admin = AdminClient({"bootstrap.servers": BOOTSTRAP_SERVERS})
-    topic = NewTopic(TOPIC, num_partitions=5, replication_factor=1)
+
+    # -------------------------------
+    # Check if topic already exists
+    # -------------------------------
+    metadata = admin.list_topics(timeout=10)
+
+    if TOPIC in metadata.topics:
+        print(f"Topic '{TOPIC}' already exists. Skipping creation.")
+        return
+    
+    topic = NewTopic(TOPIC, num_partitions=5, replication_factor=1,
+                    config={
+        "cleanup.policy": "compact",
+        "compression.type": "snappy"
+    })
 
     print(f"Creating topic: {TOPIC}")
     fs = admin.create_topics([topic])
@@ -40,9 +54,10 @@ def run_producer():
     print("Producer starting to send messages")
 
     for i in range(50):
+        key = f"key-{i % 10}"   # repeating keys to test compaction
         msg = f"Message {i}"
         print(f"[Producer] Sending: {msg}")
-        producer.produce(TOPIC, value=msg.encode(), callback=delivery_report)
+        producer.produce(TOPIC, key=key.encode(), value=msg.encode(), callback=delivery_report)
         producer.poll(0)  # Trigger callback
         time.sleep(0.1)
 
